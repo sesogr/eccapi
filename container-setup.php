4c5fcfa74d704eec1f8eb8e55bbc08599394ff8f
lygisdomhisik <lygisdomhisik@commodea.com>
2020-03-25 09:37:52 +0000

    Register FakeConsumerHandler

--- container-setup.php
+++ container-setup.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types=1);
 use Faker\Factory;
 use Faker\Generator;
+use SuiteCrmCalcApi\FakeConsumerHandler;
 use SuiteCrmCalcApi\FakeCustomerHandler;
 use SuiteCrmCalcApi\FakeEvuHandler;
 use SuiteCrmCalcApi\FakeGridProviderHandler;
@@ -11,6 +12,7 @@ return [
         return Factory::create('de_DE');
     }),
     'typeHandlers' => [
+        'consumer' => FakeConsumerHandler::class,
         'customer' => FakeCustomerHandler::class,
         'evu' => FakeEvuHandler::class,
         'gridProvider' => FakeGridProviderHandler::class,
